9044ac4bc20aa5f1d18422bc124a4e9600288fe2
Toto <marcelo@ntasistemas.com.ar>
2023-01-02 17:29:18 -0300

    Agregar archivos de proyecto.

---
+++ LectoresBAZFLP/Main.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtipdestino = New System.Windows.Forms.TextBox()
+        Me.cmdDesconectar = New System.Windows.Forms.Button()
+        Me.cmdConectar = New System.Windows.Forms.Button()
+        Me.t_placa1 = New System.Windows.Forms.Timer(Me.components)
+        Me.cmdLimpioLista = New System.Windows.Forms.Button()
+        Me.list_registros = New System.Windows.Forms.ListView()
+        Me.fechayhora = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.nrotarjeta = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.nrolector = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txttotallecturas = New System.Windows.Forms.TextBox()
+        Me.ss_status = New System.Windows.Forms.StatusStrip()
+        Me.tt_versionapi = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.t_reset = New System.Windows.Forms.Timer(Me.components)
+        Me.lblIPDestino = New System.Windows.Forms.Label()
+        Me.tsEstadoConexion = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.groupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.ss_status.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.lblIPDestino)
+        Me.groupBox1.Controls.Add(Me.txtipdestino)
+        Me.groupBox1.Controls.Add(Me.cmdDesconectar)
+        Me.groupBox1.Controls.Add(Me.cmdConectar)
+        resources.ApplyResources(Me.groupBox1, "groupBox1")
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.TabStop = False
+        '
+        'txtipdestino
+        '
+        resources.ApplyResources(Me.txtipdestino, "txtipdestino")
+        Me.txtipdestino.Name = "txtipdestino"
+        '
+        'cmdDesconectar
+        '
+        resources.ApplyResources(Me.cmdDesconectar, "cmdDesconectar")
+        Me.cmdDesconectar.Name = "cmdDesconectar"
+        Me.cmdDesconectar.UseVisualStyleBackColor = True
+        '
+        'cmdConectar
+        '
+        resources.ApplyResources(Me.cmdConectar, "cmdConectar")
+        Me.cmdConectar.Name = "cmdConectar"
+        Me.cmdConectar.UseVisualStyleBackColor = True
+        '
+        't_placa1
+        '
+        Me.t_placa1.Interval = 50
+        '
+        'cmdLimpioLista
+        '
+        resources.ApplyResources(Me.cmdLimpioLista, "cmdLimpioLista")
+        Me.cmdLimpioLista.Name = "cmdLimpioLista"
+        Me.cmdLimpioLista.UseVisualStyleBackColor = True
+        '
+        'list_registros
+        '
+        Me.list_registros.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.fechayhora, Me.nrotarjeta, Me.nrolector})
+        Me.list_registros.GridLines = True
+        Me.list_registros.HideSelection = False
+        resources.ApplyResources(Me.list_registros, "list_registros")
+        Me.list_registros.Name = "list_registros"
+        Me.list_registros.UseCompatibleStateImageBehavior = False
+        Me.list_registros.View = System.Windows.Forms.View.Details
+        '
+        'fechayhora
+        '
+        resources.ApplyResources(Me.fechayhora, "fechayhora")
+        '
+        'nrotarjeta
+        '
+        resources.ApplyResources(Me.nrotarjeta, "nrotarjeta")
+        '
+        'nrolector
+        '
+        resources.ApplyResources(Me.nrolector, "nrolector")
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.txttotallecturas)
+        Me.GroupBox2.Controls.Add(Me.list_registros)
+        Me.GroupBox2.Controls.Add(Me.cmdLimpioLista)
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.TabStop = False
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'txttotallecturas
+        '
+        resources.ApplyResources(Me.txttotallecturas, "txttotallecturas")
+        Me.txttotallecturas.Name = "txttotallecturas"
+        Me.txttotallecturas.ReadOnly = True
+        '
+        'ss_status
+        '
+        Me.ss_status.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tt_versionapi, Me.tsEstadoConexion})
+        resources.ApplyResources(Me.ss_status, "ss_status")
+        Me.ss_status.Name = "ss_status"
+        '
+        'tt_versionapi
+        '
+        Me.tt_versionapi.Name = "tt_versionapi"
+        resources.ApplyResources(Me.tt_versionapi, "tt_versionapi")
+        '
+        't_reset
+        '
+        Me.t_reset.Interval = 300000
+        '
+        'lblIPDestino
+        '
+        resources.ApplyResources(Me.lblIPDestino, "lblIPDestino")
+        Me.lblIPDestino.Name = "lblIPDestino"
+        '
+        'tsEstadoConexion
+        '
+        resources.ApplyResources(Me.tsEstadoConexion, "tsEstadoConexion")
+        Me.tsEstadoConexion.Name = "tsEstadoConexion"
+        Me.tsEstadoConexion.Spring = True
+        '
+        'Main
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ss_status)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.groupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ss_status.ResumeLayout(False)
+        Me.ss_status.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents groupBox1 As System.Windows.Forms.GroupBox
+    Private WithEvents cmdDesconectar As System.Windows.Forms.Button
+    Private WithEvents cmdConectar As System.Windows.Forms.Button
+    Private WithEvents t_placa1 As System.Windows.Forms.Timer
+    Private WithEvents cmdLimpioLista As System.Windows.Forms.Button
+    Private WithEvents list_registros As System.Windows.Forms.ListView
+    Private WithEvents fechayhora As System.Windows.Forms.ColumnHeader
+    Private WithEvents nrotarjeta As System.Windows.Forms.ColumnHeader
+    Private WithEvents nrolector As System.Windows.Forms.ColumnHeader
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtipdestino As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txttotallecturas As System.Windows.Forms.TextBox
+    Friend WithEvents ss_status As System.Windows.Forms.StatusStrip
+    Friend WithEvents tt_versionapi As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents lblIPDestino As Label
+    Friend WithEvents tsEstadoConexion As ToolStripStatusLabel
+    Friend WithEvents t_reset As Timer
+End Class
---
+++ LectoresBAZFLP/Main.vb
@@ -0,0 +1,358 @@
+﻿Imports System
+Imports System.Runtime.InteropServices
+Imports System.IO
+Imports System.Text
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Web
+Imports System.Drawing
+Imports System.Threading.Tasks
+
+Public Class Main
+
+    Private placa1 As IntPtr = IntPtr.Zero
+
+    Dim contador_lecturas As Integer
+
+    <DllImport("C:\WINDOWS\system32\plcommpro.dll", EntryPoint:="Connect")>
+    Public Shared Function Conectar(ByVal parametros As String) As IntPtr
+
+    End Function
+
+    <DllImport("plcommpro.dll", EntryPoint:="Disconnect")>
+    Public Shared Sub Desconectar(ByVal h As IntPtr)
+
+    End Sub
+
+    <DllImport("plcommpro.dll", EntryPoint:="GetRTLog")>
+    Public Shared Function GetRTLog(ByVal h As IntPtr, ByRef buffer As Byte, ByVal buffersize As Integer) As Integer
+    End Function
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'SET TITULO
+        Me.Text = "Lector de Tarjetas"
+
+        'LEO Y SETEO VERSION DE LA API
+        tt_versionapi.Text = Application.ProductVersion.ToString
+
+        'TEXTO TIMER RESET
+        tsEstadoConexion.Text = "DESCONECTADO"
+
+    End Sub
+
+    Private Async Sub CmdConectar_Click(sender As Object, e As EventArgs) Handles cmdConectar.Click
+
+        'PLACA 1
+
+        If txtipdestino.Text = "" Then
+
+        Else
+
+            Dim ipDestino As String = Trim(txtipdestino.Text)
+
+            Dim cadenaconex As String = "protocol=TCP,ipaddress=" & ipDestino & ",port=4370,timeout=2000,passwd="
+
+            Cursor = Cursors.WaitCursor
+
+            If IntPtr.Zero = placa1 Then
+
+                placa1 = Conectar(cadenaconex)
+                Cursor = Cursors.[Default]
+
+                If placa1 <> IntPtr.Zero Then
+                    t_placa1.Enabled = True 'START AL TIMER TICK
+                    cmdDesconectar.Enabled = True
+                    cmdConectar.Enabled = False
+                    tsEstadoConexion.Text = "CONECTADO"
+                    tsEstadoConexion.ForeColor = Color.DarkGreen
+                Else
+                    tsEstadoConexion.Text = "ERROR DE CONEXION"
+                    tsEstadoConexion.ForeColor = Color.Red
+                    Await BlinkText()
+                End If
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub CmdDesconectar_Click(sender As Object, e As EventArgs) Handles cmdDesconectar.Click
+
+
+        'CONEX PLACA 1
+        If IntPtr.Zero <> placa1 Then
+            Desconectar(placa1)
+            placa1 = IntPtr.Zero
+            t_placa1.Enabled = False 'STOP AL TIMER TICK
+            cmdDesconectar.Enabled = False
+            cmdConectar.Enabled = True
+            tsEstadoConexion.Text = "DESCONECTADO"
+        End If
+
+    End Sub
+
+    Private Sub CmdLimpioLista_Click(sender As Object, e As EventArgs) Handles cmdLimpioLista.Click
+
+        'LIMPIO EL DATAGRID
+        Me.list_registros.Items.Clear()
+
+        'REINICIO EL CONTADOR
+        contador_lecturas = 0
+        txttotallecturas.Text = "0"
+
+    End Sub
+
+    Private Sub Placa1_tick(sender As Object, e As EventArgs) Handles t_placa1.Tick
+        Dim buffersize As Integer = 256
+        Dim buffer As Byte() = New Byte(255) {}
+        Dim i As Integer = Me.list_registros.Items.Count
+
+        If IntPtr.Zero <> placa1 Then
+
+            Dim ret As Integer = GetRTLog(placa1, buffer(0), buffersize)
+
+            If ret >= 0 Then
+                Dim str As String = Encoding.[Default].GetString(buffer)
+                Dim tmp As String() = str.Split(","c)
+
+                Dim tipo_movi As String = tmp(4).ToString
+
+                If tipo_movi <> "255" Then
+
+                    Dim fechayhora, nrotarjeta, nrolector_inicial, nrolector_final, placa As String
+
+                    fechayhora = tmp(0).ToString 'FECHA Y HORA DE LA LECTURA
+                    nrotarjeta = tmp(2).ToString 'NRO DE LA TARJETA
+                    nrolector_inicial = tmp(3).ToString 'LECTOR
+
+
+                    'RENOMBRO PLACAS
+
+                    Dim lector As Integer = CInt(nrolector_inicial)
+
+                    Select Case lector
+
+                        Case 1 'INGRESO PEATONAL
+
+                            placa = 1
+                            nrolector_final = 1
+
+                        Case 2 'EGRESO PEATONAL
+
+                            placa = 1
+                            nrolector_final = 2
+
+                        Case 3 'INGRESO VEHICULOS LIVIANOS
+
+                            placa = 2
+                            nrolector_final = 1
+
+                        Case 4 'EGRESO VEHICULOS LIVIANOS
+
+                            placa = 2
+                            nrolector_final = 2
+
+                    End Select
+
+                    'END RENOMBRO PLACAS
+
+
+                    'FACILITY CODE
+
+                    Dim nro_leido As Integer = tmp(2)
+                    Dim nro_final As String = Binario(nro_leido)
+                    Dim facilitycode As String = nro_final.Substring(0, 8)
+                    Dim adecimal1 As Integer = ADecimal(facilitycode)
+
+                    'END FACILITY CODE
+
+
+                    'NRO TARJETA
+
+                    Dim subnrotarjeta As String = nro_final.Substring(8)
+                    Dim adecimal2 As Integer = ADecimal(subnrotarjeta)
+
+                    Dim largo As Integer = CStr(adecimal2).Length
+                    Dim intermedio As String = CStr(adecimal2)
+                    Dim cuenta As Integer = 5 - largo
+
+                    Do While cuenta > 0
+
+                        intermedio = "0" + intermedio
+                        cuenta -= 1
+
+                    Loop
+
+                    Dim nro_tarjeta As String = adecimal1.ToString + intermedio.ToString
+
+                    'END TARJETA
+
+
+                    'ENVIO A FRANCIA
+
+                    Dim let_fecha As Date = CDate(fechayhora)
+                    Dim let_placa As Integer = CInt(placa)
+                    Dim let_barrera As Integer = CInt(nrolector_final)
+                    Dim let_tarje As String = nro_tarjeta
+
+                    Dim envio_datos As String = EnvioDatos(let_fecha, let_placa, let_barrera, let_tarje)
+
+                    'FIN ENVIO A FRANCIA
+
+
+                    'AGREGO AL CUADRO
+                    Me.list_registros.Items.Add(tmp(0)) 'FECHA Y HORA
+                    Me.list_registros.Items(i).SubItems.Add(nro_tarjeta) 'NRO DE TARJETA
+
+                    Select Case tmp(3)
+
+                        Case 1
+                            Me.list_registros.Items(i).SubItems.Add("INGRESO PEATONAL") 'LECTOR
+                        Case 2
+                            Me.list_registros.Items(i).SubItems.Add("EGRESO PEATONAL") 'LECTOR
+                        Case 3
+                            Me.list_registros.Items(i).SubItems.Add("INGRESO VEHICULAR") 'LECTOR
+                        Case 4
+                            Me.list_registros.Items(i).SubItems.Add("EGRESO VEHICULAR") 'LECTOR
+
+                    End Select
+
+                    'contador
+                    contador_lecturas += 1
+                    txttotallecturas.Text = contador_lecturas.ToString
+
+                    'FIN AGREGO AL CUADRO
+
+                End If
+
+            End If
+
+        Else
+
+            Return
+            End
+
+        End If
+
+    End Sub
+
+    Public Function EnvioDatos(ByVal fecha As Date, ByVal placa As Integer, ByVal barrera As Integer, ByVal tarjeta As String)
+
+        Try
+
+            'VARIABLES
+            Dim url As String = "https://concesionaria.bazflp.com/api/socket.io/tarjTest.php"
+            Dim metodo As String = "POST"
+            Dim let_fecha As String = CDate(fecha).ToShortDateString
+            Dim let_placa As String = CStr(CInt(placa))
+            Dim let_barrera As String = barrera
+            Dim let_tarje As String = tarjeta.ToString
+            Dim pwd As String = "DcNrimAxvKKX8Th1sdX0xjZVxBZDqUW9TDpSi3p301u062LIUkiTCAH1OlHNhxhLiCb6xPmPLC7oSpsZthrJSotyw2MbbpXu1"
+
+            'METODO
+            Dim request As System.Net.WebRequest = System.Net.WebRequest.Create(url)
+            request.Method = metodo
+
+            'DATOS
+            Dim postData As String = "let_fecha=" + let_fecha + "&let_placa=" + let_placa + "&let_barrera=" + let_barrera + "&let_tarje=" + let_tarje + "&pwd=" + pwd + ""
+
+            'CODIFICACION
+            Dim byteArray As Byte() = Encoding.UTF8.GetBytes(postData)
+            request.ContentType = "application/x-www-form-urlencoded"
+            request.ContentLength = byteArray.Length
+            Dim dataStream As Stream = request.GetRequestStream()
+            dataStream.Write(byteArray, 0, byteArray.Length)
+            dataStream.Close()
+
+            'RESPUESTA
+            Dim respuesta As WebResponse = request.GetResponse()
+            dataStream = respuesta.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+            reader.Close()
+            dataStream.Close()
+            respuesta.Close()
+            Return (responseFromServer)
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Function
+
+    Function Binario(ByVal valor As Integer) As String
+
+        Dim mayor As Integer
+        Dim retorno As String
+        Dim a As Integer
+
+        'Busca el mayor exponente
+        mayor = 0
+        Do While True
+            If 2 ^ mayor > valor Then
+                If mayor > 0 Then
+                    mayor -= 1
+                End If
+                Exit Do
+            End If
+            mayor += 1
+        Loop
+
+        'Calculo del valor binario
+        retorno = ""
+        For a = mayor To 0 Step -1
+            If valor < (2 ^ a) Then
+                retorno &= "0"
+            Else
+                retorno &= "1"
+                valor -= (2 ^ a)
+            End If
+        Next a
+
+        Dim largo As Integer = retorno.Length
+        Dim cuenta As Integer = 24 - largo
+
+        Do While cuenta > 0
+
+            retorno = "0" + retorno
+            cuenta -= 1
+
+        Loop
+
+        'Pasa el valor como resultado de la función
+        Binario = retorno
+
+    End Function
+
+    Function ADecimal(ByVal valor As String) As Integer
+
+        valor = StrReverse(valor)
+        Dim largo As Integer = Len(valor)
+        Dim x, suma As Integer
+
+        For I = 1 To largo
+            x = 2 ^ (I - 1)
+            suma += (Int(Mid$(valor, I, 1)) * x)
+        Next I
+        Return suma
+
+    End Function
+
+    Async Function BlinkText() As Task
+
+        For i = 0 To 10
+            Await Task.Delay(500)
+            tsEstadoConexion.Visible = Not tsEstadoConexion.Visible
+        Next
+
+        tsEstadoConexion.Visible = True
+
+    End Function
+
+End Class
+
+
+
